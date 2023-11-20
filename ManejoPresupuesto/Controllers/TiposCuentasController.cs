4bd440f3599bbeae16649f166f9c9d0b7f7dce71
Sylar-1 <alex1998u@hotmail.com>
2023-11-19 20:51:35 -0500

    Se agrego Tipos Cuentas Controlador, Modelo y Vista Crear

---
+++ ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using ManejoPresupuesto.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class TiposCuentasController: Controller
+    {
+        public TiposCuentasController() 
+        {
+            
+        }
+        public IActionResult Crear()
+        {
+            using (var conection = new SqlConnection(connectionString)) 
+            {
+                var query = conection.Query("SELECT 1").FirstOrDefault();
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Crear(TipoCuenta tipoCuenta)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(tipoCuenta);
+            }
+
+            return View();
+        }
+    }
+}
